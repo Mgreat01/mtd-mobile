3427cfb4f697f940d6d38485786ae25aba1f8d9e
Exauce Bodi <exaucebodi4@gmail.com>
2025-12-04 14:33:01 +0100

    feat : ajout des methodes pour sauvegarder, recuperer et supprimer l'utilisateur en local
    feat : ajout de la methode deleteUser (User local)
    
    feat : ajout de la methode pour stocker l'user en local
    
    feat : ajout de la methode pour recuperer l'user local

--- lib/framework/user/userLocalServiceImpl.dart
+++ lib/framework/user/userLocalServiceImpl.dart
@@ -1,23 +1,36 @@
+import 'dart:convert';
+import 'package:get_storage/get_storage.dart';
 import 'package:moto_taxi_digital_mobile/business/models/user/user.dart';
 import 'package:moto_taxi_digital_mobile/business/services/user/userLocalService.dart';
 
-class UserLocalServiceImpl extends UserLocalService {
+class UserLocalServiceImpl implements UserLocalService {
+  GetStorage? box;
+
+  UserLocalServiceImpl({this.box});
+
   @override
-  Future<bool> deleteUser() {
-    // TODO: implement deleteUser
-    throw UnimplementedError();
+  Future<bool> deleteUser() async{
+    await box?.remove("user");
+    return true ;
   }
 
   @override
-  Future<User?> getUser() {
-    // TODO: implement getUser
-    throw UnimplementedError();
+  Future<User?> getUser() async{
+    var userJson = await box?.read("user");
+    if(userJson == null ){
+      return null;
+    }
+    var user = User.fromJson(userJson);
+
+    return user;
   }
 
   @override
-  Future<bool> saveUser(User user) {
-    // TODO: implement saveUser
-    throw UnimplementedError();
+  Future<bool> saveUser(User user) async{
+    var data = user.toJson();
+    await box?.write("user", jsonEncode(data));
+    return true;
+
   }
 
 }

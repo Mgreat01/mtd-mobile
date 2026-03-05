1e1ab91018b149807e5cfd6b3287378c40ab8b32
Exauce Bodi <exaucebodi4@gmail.com>
2026-03-05 21:05:35 +0100

    feat : ajout de l'attribut user dans loginState

--- lib/pages/login/loginState.dart
+++ lib/pages/login/loginState.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/foundation.dart';
+import 'package:moto_taxi_digital_mobile/business/models/user/user.dart';
 
 @immutable
 class LoginState {
   final bool isLoading;
   final String? error;
   final bool isSuccess;
+  final User? user;
 
   const LoginState({
     this.isLoading = false,
     this.error,
     this.isSuccess = false,
+    this.user,
   });
 
   LoginState copyWith({
     bool? isLoading,
     String? error,
     bool? isSuccess,
+    User? user
   }) {
     return LoginState(
       isLoading: isLoading ?? this.isLoading,
       error: error,
       isSuccess: isSuccess ?? this.isSuccess,
+      user: user ?? this.user
     );
   }
 }

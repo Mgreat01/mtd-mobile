afb9dc124baa3aabb163be7cdb136eeaa2437fc4
Mgreat01 <ephraimmonga5@gmail.com>
2026-03-19 16:20:56 +0100

    feat : creation du model result

---
+++ lib/business/models/searchResult/searchResult.dart
@@ -0,0 +1,7 @@
+import 'package:latlong2/latlong.dart';
+
+class SearchResult {
+  final LatLng location;
+  final String displayName;
+  SearchResult({required this.location, required this.displayName});
+}
